4723df041779105b8d7fb3e982566202e6eba19b
Kunal Gupta <kunal.g@surya-soft.com>
2021-04-15 14:27:38 +0530

    Update

--- index.js
+++ index.js
@@ -29,11 +29,7 @@ async function listAllTags(octokit, owner, repo) {
 
 async function run() {
 	const token = core.getInput('GH_TOKEN');
-	var tagPrefix = core.getInput('TAG_PREFIX');
-	if (!tagPrefix) {
-		tagPrefix = 'v';
-	}
-	// const octokit = new github.GitHub(token);
+  
   const octokit = github.getOctokit(token);
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
@@ -42,56 +38,16 @@ async function run() {
 
 	const owner = env.GITHUB_REPOSITORY.split('/')[0];
 	const repo = env.GITHUB_REPOSITORY.split('/')[1];
-	const commit = await octokit.pulls.get({
+	const { data: pullRequest } = await octokit.pulls.get({
 		owner,
 		repo,
-		issue_number: pull_request.number
+		pull_number: pull_request.number,
+		mediaType: {
+			format: 'diff'
+		}
 	});
-	core.info(JSON.stringify(commit));
-	const versionTagRegex = new RegExp(`^${tagPrefix}(\\d+)\\.(\\d+)\\.(\\d+)$`);
+	core.info(JSON.stringify(pullRequest));
 
-	const allTags = await listAllTags(octokit, owner, repo);
-	const tags = allTags.filter((el) => el.match(versionTagRegex));
-
-	if (tags.length < 1) {
-		setBuildVersion('0.0.1');
-		return;
-	}
-	core.error(JSON.stringify(tags));
-
-	tags.sort((l, r) => {
-		const lx = parseInt(l.split('.')[0]);
-		const rx = parseInt(r.split('.')[0]);
-		if (lx < rx) {
-			return 1;
-		}
-		if (rx < lx) {
-			return -1;
-		}
-		const ly = parseInt(l.split('.')[1]);
-		const ry = parseInt(r.split('.')[1]);
-		if (ly < ry) {
-			return 1;
-		}
-		if (ry < ly) {
-			return -1;
-		}
-		const lz = parseInt(l.split('.')[2]);
-		const rz = parseInt(r.split('.')[2]);
-		if (lz < rz) {
-			return 1;
-		}
-		if (rz < lz) {
-			return -1;
-		}
-		return 0;
-	});
-	const split = tags[0].substring(tagPrefix.length).split('.');
-	const nextX = parseInt(split[0]);
-	const nextY = parseInt(split[1]);
-	const nextZ = parseInt(split[2]) + 1;
-
-	setBuildVersion(`${nextX}.${nextY}.${nextZ}`);
 }
 
 run();

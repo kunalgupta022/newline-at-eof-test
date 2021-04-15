058e7bfe2975bfeabf063fb7418d8758d9645b1b
kunalgupta022 <kunalgupta022@users.noreply.github.com>
2021-04-15 09:10:25 +0000

    Generate distribution

--- dist/index.js
+++ dist/index.js
@@ -9,39 +9,12 @@ const core = __nccwpck_require__(186);
 const github = __nccwpck_require__(438);
 const env = process.env;
 
-function setBuildVersion(buildVersion) {
-	core.setOutput('NEXT_BUILD_VERSION', buildVersion);
-}
-
-async function listAllTags(octokit, owner, repo) {
-	var page = 1;
-	var tags = [];
-	while (true) {
-		const response = await octokit.repos.listTags({
-			owner,
-			repo,
-			per_page: 100,
-			page: page
-		});
-		const newTags = response['data'].map((obj) => obj['name']);
-
-		if (!newTags.length) {
-			break;
-		}
-		tags.push(...newTags);
-		page++;
-	}
-	return tags;
-}
-
 async function run() {
 	const token = core.getInput('GH_TOKEN');
-  
-  const octokit = github.getOctokit(token);
+
+	const octokit = github.getOctokit(token);
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
-
-  core.info(JSON.stringify(pull_request));
 
 	const owner = env.GITHUB_REPOSITORY.split('/')[0];
 	const repo = env.GITHUB_REPOSITORY.split('/')[1];
@@ -50,11 +23,10 @@ async function run() {
 		repo,
 		pull_number: pull_request.number,
 		mediaType: {
-			format: 'diff'
+			format: 'patch'
 		}
 	});
 	core.info(JSON.stringify(pullRequest));
-
 }
 
 run();

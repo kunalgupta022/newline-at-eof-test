54a2d300cc470c463f0857fef7de295b56b7f452
Kunal Gupta <kunal.g@surya-soft.com>
2021-04-17 17:50:45 +0530

    Able to remove trailing whitspaces

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const {parse: gitDiffParser} = require('what-the-diff');
+const { parse: gitDiffParser } = require('what-the-diff');
 const fs = require('fs');
 const env = process.env;
 
@@ -9,32 +9,23 @@ function matchExact(r, str) {
 	return match && str === match[0];
 }
 
-function endOfFileRule(b) {
-
-	// for empty files
-	if(b.length < 1 ){
-		return true
+function stripTrailingSpaces(b) {
+	// If file is empty
+	if (b.length < 1) {
+		return b;
 	}
-  var valid = false;
-	if (b.length >= 1) {
-		valid = /[^\n]\n\z/.test(b)
-	} else {
-		valid = /\n\z/.test(b)
+	// If file is not empty
+	b = b.replace(/[ \t\n]*$/, '\n');
+
+	if (b.length === 1) {
+		// if the remaining character is a space
+		if (/[ \t\n]*$/.test(b)) {
+			return '';
+		} else {
+			return b + '\n';
+		}
 	}
-
-	if(valid){
-		return valid
-	}
-
-	// add one new line to the end of file
-	fix = RegExp("(.)\z").ReplaceAll(b, []byte("$1\n"))
-
-	if rule.SingleNewLine {
-		// rm extra new lines, if any
-		fix = regexp.MustCompile(`\n+\z`).ReplaceAll(fix, []byte{'\n'})
-	}
-
-	return valid, fix
+	return b;
 }
 
 async function run() {
@@ -62,12 +53,14 @@ async function run() {
 		}
 	});
 	const parsedDiff = gitDiffParser(pullRequestDiff);
+
 	const changedFilePaths = parsedDiff.map((e) => {
 		return e['newPath'];
 	});
+
 	core.info(JSON.stringify(changedFilePaths));
 
-	var filesToCheck = changedFilePaths.map((e) => {
+	let filesToCheck = changedFilePaths.map((e) => {
 		for (var i = 0; i < ignorePaths.length; i++) {
 			if (matchExact(ignorePaths[i], e)) {
 				return null;
@@ -75,22 +68,28 @@ async function run() {
 		}
 		return e;
 	});
+
 	core.info(JSON.stringify(filesToCheck));
-	var data = fs.readFileSync('index.js', "utf8");
-	console.log(JSON.stringify(data));
-	core.info(JSON.stringify(data));
+
+
 	for (var i = 0; i < filesToCheck.length; i++) {
 		try {
-			var data = fs.readFileSync(filesToCheck[i], 'utf8');
-			core.info(data);
-			// if (filesToCheck[i] !== null) {
-			// 	var data = fs.readFileSync(filesToCheck[i], 'utf8');
-			// 	core.info(data);
-			// }
+			if (filesToCheck[i] !== null) {
+				let data = fs.readFileSync(filesToCheck[i], {
+					encoding: 'utf8',
+					flag: 'r'
+				});
+				core.info(data);
+				data = stripTrailingSpaces(data);
+				fs.writeFileSync(filesToCheck[i], data, 'utf8');
+        core.info(data);
+			}
 		} catch (e) {
 			core.error('Error:', e.stack);
 		}
 	}
+
+
 }
 
 run();

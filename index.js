4d0cefa2a7edf05d3793b1263a3685911928cbf8
Kunal Gupta <kunal.g@surya-soft.com>
2021-04-17 18:40:07 +0530

    Commit the cahnged files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const exec = require('@actions/exec');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { parse: gitDiffParser } = require('what-the-diff');
-const fs = require('fs');
+const fs = require('fs-extra');
+const simpleGit = require('simple-git');
 const env = process.env;
 
 function matchExact(r, str) {
@@ -38,6 +40,23 @@ async function run() {
 			return e;
 		}
 	});
+
+	const url = `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}.git`.replace(
+		/^https:\/\//,
+		`https://x-access-token:${token}@`
+	);
+	var branch;
+	if (github.context.eventName == 'pull_request') {
+		branch = github.context.payload.pull_request.head.ref;
+	} else {
+		branch = github.context.ref.replace('refs/heads/', '');
+	}
+
+	const git = simpleGit();
+	await git.addRemote('repo', url);
+	await git.fetch('repo');
+	await git.checkout(branch);
+
 	const octokit = github.getOctokit(token);
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
@@ -52,11 +71,11 @@ async function run() {
 			format: 'diff'
 		}
 	});
-  core.info(JSON.stringify(pullRequestDiff));
+	core.info(JSON.stringify(pullRequestDiff));
 	const parsedDiff = gitDiffParser(pullRequestDiff);
 
 	const changedFilePaths = parsedDiff.map((e) => {
-		return e['newPath'].replace('b/','');
+		return e['newPath'].replace('b/', '');
 	});
 
 	core.info('changedFilePaths ' + JSON.stringify(changedFilePaths));
@@ -72,25 +91,37 @@ async function run() {
 
 	core.info(JSON.stringify(filesToCheck));
 
-
 	for (var i = 0; i < filesToCheck.length; i++) {
-		try {
-			if (filesToCheck[i] !== null) {
-				let data = fs.readFileSync(filesToCheck[i], {
-					encoding: 'utf8',
-					flag: 'r'
-				});
-				core.info(data);
-				data = stripTrailingSpaces(data);
-				fs.writeFileSync(filesToCheck[i], data, 'utf8');
-        core.info(data);
-			}
-		} catch (e) {
-			core.error('Error:', e.stack);
+		if (filesToCheck[i] !== null) {
+			let data = fs.readFileSync(filesToCheck[i], {
+				encoding: 'utf8',
+				flag: 'r'
+			});
+			core.info(data);
+			data = stripTrailingSpaces(data);
+			fs.writeFileSync(filesToCheck[i], data, 'utf8');
+			core.info(data);
 		}
 	}
 
+	const diff = await exec.exec('git', ['diff', '--quiet'], {
+		ignoreReturnCode: true
+	});
 
+	if (diff) {
+		await core.group('push changes', async () => {
+			await git.addConfig(
+				'user.email',
+				`${env.GITHUB_ACTOR}@users.noreply.github.com`
+			);
+			await git.addConfig('user.name', env.GITHUB_ACTOR);
+			await git.add('./');
+			await git.commit('Fixed Trailing Whitespaces and EOF Newline');
+			await git.push('repo', branch);
+		});
+	} else {
+		console.log('No changes to make');
+	}
 }
 
 run();

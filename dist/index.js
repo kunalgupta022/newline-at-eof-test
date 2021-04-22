d596e7615f2ae27320154d1410c0785838bcfac7
kunalgupta022 <kunalgupta022@users.noreply.github.com>
2021-04-22 10:46:35 +0000

    Generate distribution

--- dist/index.js
+++ dist/index.js
@@ -117,6 +117,10 @@ async function run() {
       });
       core.info(filesToCheck[i]);
       const fixedData = fixNewLineEOF(data);
+      if (filesToCheck[i] === "package.json"){
+        core.info(fixedData);
+        core.info(data);
+      }
       if (data !== fixedData) {
         filesToCommit.push(filesToCheck[i]);
         core.info(filesToCommit);
@@ -153,10 +157,6 @@ async function run() {
 
 run();
 
-
-
-
-     
 
 /***/ }),
 
